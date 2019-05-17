bd0c4986b2c758a0782463ee389dd48b047d6493
NithishB <nithish.mdh@gmail.com>
2019-05-17 18:24:25 +0530

    Updates

---
+++ precache-manifest.0f69ac5b2caf1383c8064cbe72de01cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5d4fdb90c7c2877438489fb7fdc77b",
+    "url": "/deeptrace-react-app/index.html"
+  },
+  {
+    "revision": "fc993614f70d15333da6",
+    "url": "/deeptrace-react-app/static/css/main.7702184e.chunk.css"
+  },
+  {
+    "revision": "107078a3ca495498a66f",
+    "url": "/deeptrace-react-app/static/js/2.e3769a7e.chunk.js"
+  },
+  {
+    "revision": "fc993614f70d15333da6",
+    "url": "/deeptrace-react-app/static/js/main.d9ea8fca.chunk.js"
+  },
+  {
+    "revision": "9871cbe1e4271dce38ff",
+    "url": "/deeptrace-react-app/static/js/runtime~main.5c2b3156.js"
+  }
+]);
